b2b8d58ee0eb5310c3ccb89e786c2448d45127c4
Jacob <jacoblittler2003@gmail.com>
2025-12-29 15:15:12 -0500

    Recursive case

--- fib.rb
+++ fib.rb
@@ -7,4 +7,15 @@ def fibs(n)
   return array
 end
 
-puts fibs(8)
+# puts fibs(1)
+
+def fibs_rec(n) 
+  return [] if n == 0
+  return [0] if n == 1
+  return [0, 1] if n == 2
+
+  prev = fibs_rec(n - 1)
+  prev << prev[-1] + prev[-2]
+end
+
+puts fibs_rec(8)
